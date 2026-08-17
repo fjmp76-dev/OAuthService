d6aabb9853e30e789fea4088f5d844cfdbb0e10f
fjmp76-dev <fjmp76@gmail.com>
2026-08-17 19:14:44 -0400

    ++

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://plexrag-api.fjmp76.com',
-  googleClientId: '241613390950-2g2psec2f71t331k8e4re60ns5ruv08c.apps.googleusercontent.com',
+  googleClientId: '241613390950-09j76d93k6gd0icfbqodnno9apk1luf4.apps.googleusercontent.com',
   microsoftClientId: '10702fe0-d01f-4997-b9b9-d66f15671d41'
 };
